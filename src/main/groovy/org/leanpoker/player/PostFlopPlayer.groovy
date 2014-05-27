2788dcec06911352c82f4763014caf60e61a523b
xza <xza@localhost>
2014-05-27 23:53:40 +0200

    ranking API

--- src/main/groovy/org/leanpoker/player/PostFlopPlayer.groovy
+++ src/main/groovy/org/leanpoker/player/PostFlopPlayer.groovy
@@ -1,9 +1,13 @@
 package org.leanpoker.player
 
 import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
 import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.ContentType
+import groovyx.net.http.RESTClient
 
 import static groovyx.net.http.ContentType.JSON
+import static groovyx.net.http.ContentType.URLENC
 import static groovyx.net.http.Method.POST
 
 /**
@@ -13,6 +17,8 @@ class PostFlopPlayer extends Player
 {
     static int betAmount(final game)
     {
+        if (ranking(game) > 3)
+            return ALLIN;
         return 0;
     }
 
@@ -31,24 +37,18 @@ class PostFlopPlayer extends Player
         return ret
     }
 
-    static void ranking(game)
+    static Object ranking(game)
     {
         def cards = allCards(game)
         def cardString = new JsonBuilder(cards).toString()
 
+        def api = new RESTClient( 'http://localhost:2048/' )
 
-        def http = new HTTPBuilder('http://localhost:2048/')
+        def resp = api.post(
+                body: [cards: cardString],
+                requestContentType : URLENC
+        )
 
-        http.request(POST, JSON) { req ->
-
-            body = [
-                    cards: cardString
-            ]
-
-            response.success = { resp, json ->
-                println 'YAY!'
-            }
-        }
-
+        return new JsonSlurper().parseText(resp.data)
     }
 }
--- src/test/groovy/org/leanpoker/player/PlayerFileTestSpock.groovy
+++ src/test/groovy/org/leanpoker/player/PlayerFileTestSpock.groovy
@@ -8,20 +8,10 @@ class PlayerFileTestSpock extends Specification
 {
 
     def json = new File("src/test/resources/game.json").getText()
-
-    def "JsonSlurper from File"()
-    {
-        when:
-        def game = new JsonSlurper().parseText(json)
-
-        then:
-        game.small_blind == 10
-    }
+    def game = new JsonSlurper().parseText(json)
 
     def "I am in action"()
     {
-        given:
-        def game = new JsonSlurper().parseText(json)
         when:
         def Object me = game.players[game.in_action]
         then:
@@ -31,7 +21,6 @@ class PlayerFileTestSpock extends Specification
     def "All cards"()
     {
         given:
-        def game = new JsonSlurper().parseText(json)
         def Object me = game.players[game.in_action]
         when:
         def communityCards = game.community_cards
@@ -47,13 +36,19 @@ class PlayerFileTestSpock extends Specification
     def "holecards to json"()
     {
         given:
-        def game = new JsonSlurper().parseText(json)
         def allCards = PostFlopPlayer.allCards(game)
         when:
         def builder = new JsonBuilder(allCards)
         def string = builder.toString()
         then:
         string == '[{"rank":"4","suit":"spades"},{"rank":"A","suit":"hearts"},{"rank":"6","suit":"clubs"},{"rank":"3","suit":"hearts"},{"rank":"K","suit":"spades"}]'
+    }
+
+    def "ranking available"() {
+        when:
+        def ranking = PostFlopPlayer.ranking(game)
+        then:
+        ranking.rank == 0
     }
 
 
