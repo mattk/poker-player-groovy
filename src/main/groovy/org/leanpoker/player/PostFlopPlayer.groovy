c6730491a140fd13e33cc1e78179277131b8ca95
xza <xza@localhost>
2014-05-27 22:48:05 +0200

    all cards

--- src/main/groovy/org/leanpoker/player/PostFlopPlayer.groovy
+++ src/main/groovy/org/leanpoker/player/PostFlopPlayer.groovy
@@ -1,21 +1,54 @@
 package org.leanpoker.player
 
-import groovy.json.JsonSlurper
+import groovy.json.JsonBuilder
+import groovyx.net.http.HTTPBuilder
+
+import static groovyx.net.http.ContentType.JSON
+import static groovyx.net.http.Method.POST
 
 /**
  * @author xza
  */
 class PostFlopPlayer extends Player
 {
-    static int betAmount(final game) {
+    static int betAmount(final game)
+    {
         return 0;
     }
 
-    static Object allCards(game)
+    static List allCards(game)
     {
         def Object me = game.players[game.in_action]
         def communityCards = game.community_cards
         def holeCards = me.hole_cards
-        return communityCards + holeCards
+        def allCards = communityCards + holeCards
+
+        def ret = []
+        allCards.each { card ->
+            ret.add([rank: card.rank, suit: card.suit])
+        }
+
+        return ret
+    }
+
+    static void ranking(game)
+    {
+        def cards = allCards(game)
+        def cardString = new JsonBuilder(cards).toString()
+
+
+        def http = new HTTPBuilder('http://localhost:2048/')
+
+        http.request(POST, JSON) { req ->
+
+            body = [
+                    cards: cardString
+            ]
+
+            response.success = { resp, json ->
+                println 'YAY!'
+            }
+        }
+
     }
 }
--- src/test/groovy/org/leanpoker/player/PlayerFileTestSpock.groovy
+++ src/test/groovy/org/leanpoker/player/PlayerFileTestSpock.groovy
@@ -1,5 +1,6 @@
 package org.leanpoker.player
 
+import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
 import spock.lang.Specification
 
@@ -35,9 +36,24 @@ class PlayerFileTestSpock extends Specification
         when:
         def communityCards = game.community_cards
         def holeCards = me.hole_cards
+        def allCards = PostFlopPlayer.allCards(game)
         then:
         communityCards.size == 3
         holeCards.size == 2
+        allCards.size == 5
+    }
+
+
+    def "holecards to json"()
+    {
+        given:
+        def game = new JsonSlurper().parseText(json)
+        def allCards = PostFlopPlayer.allCards(game)
+        when:
+        def builder = new JsonBuilder(allCards)
+        def string = builder.toString()
+        then:
+        string == '[{"rank":"4","suit":"spades"},{"rank":"A","suit":"hearts"},{"rank":"6","suit":"clubs"},{"rank":"3","suit":"hearts"},{"rank":"K","suit":"spades"}]'
     }
 
 
